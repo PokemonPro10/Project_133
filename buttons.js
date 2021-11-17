742c58a8675a360fb2076531856995ec46902049
PokemonPro10 <75942534+PokemonPro10@users.noreply.github.com>
2021-11-17 20:19:15 +0530

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,19 @@
+function Bedroom(){
+    window.location="Bedroom.html";
+}
+
+function TV(){
+    window.location="TV.html";
+}
+
+function People(){
+    window.location="People.html";
+}
+
+function DogCat(){
+    window.location="DogCat.html";
+}
+
+function Tree(){
+    window.location="Tree.html";
+}
